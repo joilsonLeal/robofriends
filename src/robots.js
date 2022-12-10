ec64e80e73c572c3a8104a2901d77c15958414c5
Joilson <joilson_rg@hotmail.com>
2022-12-10 06:29:47 -0300

    feat: add card component

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Liz Campos",
+    username: "Liz",
+    email: "liz.campos@sinalmanaus.com.br",
+  },
+  {
+    id: 2,
+    name: "Stefany Bianca Aragão",
+    username: "Stefany",
+    email: "stefany.bianca@tpltransportes.com.br",
+  },
+  {
+    id: 3,
+    name: "Pedro Brito",
+    username: "Pèdro",
+    email: "pedro.brito@valeparaibano.com.br",
+  },
+  {
+    id: 4,
+    name: "Luiz Rodrigues",
+    username: "Luiz",
+    email: "luiz.rodrigues@europamotors.com.br",
+  },
+  {
+    id: 5,
+    name: "Lavínia Sales",
+    username: "Lavínia",
+    email: "lavinia.sales@sicredi.com.br",
+  },
+];
