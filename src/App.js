aa743caa250d721cc38bc655f8eba04cb2d84ac8
Joilson <joilson_rg@hotmail.com>
2022-12-10 15:05:29 -0300

    feat: add app style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -28,9 +34,13 @@ class App extends React.Component {
       );
     });
 
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading</h1>;
+    }
+
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
