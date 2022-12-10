9c7d3c5969192d809095c38695188909071639ab
Joilson <joilson_rg@hotmail.com>
2022-12-10 18:55:43 -0300

    refactor: use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => setRobots(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
-      return (
-        robot.name
-          .toLowerCase()
-          .includes(this.state.searchfield?.toLowerCase()) ||
-        robot.email
-          .toLowerCase()
-          .includes(this.state.searchfield?.toLowerCase())
-      );
-    });
-
-    if (this.state.robots.length === 0) {
-      return <h1 className="tc">Loading</h1>;
-    }
-
+  const filteredRobots = robots.filter((robot) => {
     return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <ErrorBoundry>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </ErrorBoundry>
-      </div>
+      robot.name.toLowerCase().includes(searchfield?.toLowerCase()) ||
+      robot.email.toLowerCase().includes(searchfield?.toLowerCase())
     );
-  }
+  });
+
+  return !robots.length ? (
+    <h1 className="tc">Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <ErrorBoundry>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </ErrorBoundry>
+    </div>
+  );
 }
 
 export default App;
